8c43e73a906250e521b99fd2e31f949d2b2e9783
Bogdan Opanchuk <bogdan@opanchuk.net>
2019-12-05 22:03:59 -0800

    algo: some internal name changes in the wake of power_of_2() hiding

--- src/algo.jl
+++ src/algo.jl
@@ -42,23 +42,23 @@ function Base.inv(mk::MultiplicationKey, cipher::Ciphertext, log_precision::Int,
 end
 
 
-function power_of_2_extended(
-        mk::MultiplicationKey, cipher::Ciphertext, log_precision::Int, log_degree::Int)
-    res = Array{Ciphertext}(undef, log_degree + 1)
+function power_bin_exp_extended(
+        mk::MultiplicationKey, cipher::Ciphertext, log_precision::Int, log_pwr::Int)
+    res = Array{Ciphertext}(undef, log_pwr + 1)
     res[1] = cipher
-    for i in 1:log_degree
+    for i in 1:log_pwr
         res[i+1] = rescale_by(square(mk, res[i]), log_precision)
     end
     res
 end
 
 
-function power_extended(mk::MultiplicationKey, cipher::Ciphertext, log_precision::Int, degree::Int)
-    log_degree = floor(Int, log2(degree))
-    cpows = power_of_2_extended(mk, cipher, log_precision, log_degree)
-    res = Array{Ciphertext}(undef, degree)
+function power_extended(mk::MultiplicationKey, cipher::Ciphertext, log_precision::Int, pwr::Int)
+    log_pwr = num_bits(pwr) - 1
+    cpows = power_bin_exp_extended(mk, cipher, log_precision, log_pwr)
+    res = Array{Ciphertext}(undef, pwr)
     idx = 0
-    for i in 0:log_degree-1
+    for i in 0:log_pwr-1
         powi = 1 << i
         res[idx+1] = cpows[i+1]
         idx += 1
@@ -70,13 +70,13 @@ function power_extended(mk::MultiplicationKey, cipher::Ciphertext, log_precision
             idx += 1
         end
     end
-    res[idx+1] = cpows[log_degree+1]
+    res[idx+1] = cpows[log_pwr+1]
     idx += 1
-    degree2 = 1 << log_degree
-    for i in 0:degree-degree2-1
-        bits_down = res[i+1].log_cap - cpows[log_degree+1].log_cap
+    pwr2 = 1 << log_pwr
+    for i in 0:pwr-pwr2-1
+        bits_down = res[i+1].log_cap - cpows[log_pwr+1].log_cap
         res[idx+1] = mod_down_by(res[i+1], bits_down)
-        res[idx+1] = mul(mk, res[idx+1], cpows[log_degree+1])
+        res[idx+1] = mul(mk, res[idx+1], cpows[log_pwr+1])
         res[idx+1] = rescale_by(res[idx+1], log_precision)
         idx += 1
     end
@@ -86,14 +86,14 @@ end
 
 function power_series_eager(
         mk::MultiplicationKey, cipher::Ciphertext, log_precision::Int,
-        coeffs::Array{Float64, 1}, degree::Int)
+        coeffs::Array{Float64, 1}, max_pwr::Int)
 
-    cpows = power_extended(mk, cipher, log_precision, degree)
+    cpows = power_extended(mk, cipher, log_precision, max_pwr)
 
     res = mul_by_const(cpows[1], coeffs[2], log_precision)
     res = add_const(res, coeffs[1])
 
-    for i in 1:degree-1
+    for i in 1:max_pwr-1
         if abs(coeffs[i + 2]) > 1e-27 # TODO: why this limit?
             aixi = mul_by_const(cpows[i+1], coeffs[i + 2], log_precision)
             res = mod_down_to(res, aixi.log_cap)
@@ -106,14 +106,14 @@ end
 
 function power_series_lazy(
         mk::MultiplicationKey, cipher::Ciphertext, log_precision::Int,
-        coeffs::Array{Float64, 1}, degree::Int)
+        coeffs::Array{Float64, 1}, max_pwr::Int)
 
-    cpows = power_extended(mk, cipher, log_precision, degree)
+    cpows = power_extended(mk, cipher, log_precision, max_pwr)
 
     res = mul_by_const(cpows[1], coeffs[2], log_precision)
     res = add_const(res, coeffs[1])
 
-    for i in 1:degree-1
+    for i in 1:max_pwr-1
         if abs(coeffs[i + 2]) > 1e-27 # TODO: why this limit?
             aixi = mul_by_const(cpows[i+1], coeffs[i + 2], log_precision)
             res = mod_down_by(res, res.log_cap - aixi.log_cap)
@@ -125,27 +125,27 @@ function power_series_lazy(
 end
 
 
-power_series(mk, cipher, log_precision, coeffs, degree; lazy::Bool=false) =
+power_series(mk, cipher, log_precision, coeffs, max_pwr; lazy::Bool=false) =
     lazy ?
-        power_series_lazy(mk, cipher, log_precision, coeffs, degree) :
-        power_series_eager(mk, cipher, log_precision, coeffs, degree)
+        power_series_lazy(mk, cipher, log_precision, coeffs, max_pwr) :
+        power_series_eager(mk, cipher, log_precision, coeffs, max_pwr)
 
 
-log_plus_one(mk, cipher, log_precision, degree; lazy::Bool=false) =
+log_plus_one(mk, cipher, log_precision, max_pwr; lazy::Bool=false) =
     power_series(
         mk, cipher, log_precision, [0, 1, -0.5, 1/3, -1/4, 1/5, -1/6, 1/7, -1/8, 1/9, -1/10],
-        degree, lazy=lazy)
+        max_pwr, lazy=lazy)
 
 
-Base.exp(mk, cipher, log_precision, degree; lazy::Bool=false) =
+Base.exp(mk, cipher, log_precision, max_pwr; lazy::Bool=false) =
     power_series(
         mk, cipher, log_precision,
         [1, 1, 0.5, 1/6, 1/24, 1/120, 1/720, 1/5040, 1/40320, 1/362880, 1/3628800],
-        degree, lazy=lazy)
+        max_pwr, lazy=lazy)
 
 
-sigmoid(mk, cipher, log_precision, degree; lazy::Bool=false) =
+sigmoid(mk, cipher, log_precision, max_pwr; lazy::Bool=false) =
     power_series(
         mk, cipher, log_precision,
         [1/2, 1/4, 0, -1/48, 0, 1/480, 0, -17/80640, 0, 31/1451520, 0],
-        degree, lazy=lazy)
+        max_pwr, lazy=lazy)
